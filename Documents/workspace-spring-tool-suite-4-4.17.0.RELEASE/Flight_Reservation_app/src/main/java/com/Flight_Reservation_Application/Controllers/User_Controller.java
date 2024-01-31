8008bc93b7394faefc8f48de1b45ce9f90dfefdd
Tasleem <tasleemnsheikh18@gmail.com>
2024-01-31 23:04:42 +0530

    Updated User Controller to fetch data from registration form.

--- Documents/workspace-spring-tool-suite-4-4.17.0.RELEASE/Flight_Reservation_app/src/main/java/com/Flight_Reservation_Application/Controllers/User_Controller.java
+++ Documents/workspace-spring-tool-suite-4-4.17.0.RELEASE/Flight_Reservation_app/src/main/java/com/Flight_Reservation_Application/Controllers/User_Controller.java
@@ -1,8 +1,33 @@
 package com.Flight_Reservation_Application.Controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.Flight_Reservation_Application.Entities.User;
+import com.Flight_Reservation_Application.Repositories.UserRepository;
 
 @Controller
-public class Flight_Reservation_Controller {
-
+public class User_Controller {
+	@Autowired
+	UserRepository userRepository;
+	
+	@RequestMapping("/showRegistration")
+	public String ShowReg() {
+		return "login/showRegistration";
+	}
+	
+	
+	
+	@PostMapping("/register")
+	private String Register(@ModelAttribute("user") User user,ModelMap modelMap) {
+		userRepository.save(user);
+		modelMap.addAttribute("msg","registered successfully");
+		
+		return "login/showRegistration";
+	}
 }
